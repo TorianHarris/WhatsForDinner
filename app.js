3c4b1cc637c4a3bace95860a76e856c6700c7aab
Tiana <wiggins.tiana@gmail.com>
2019-01-12 11:34:31 -0500

    added formatted bootstrap

---
+++ app.js
@@ -0,0 +1,10 @@
+//create global variables for stored ingredient entries
+let ingredient;
+let newIngredient;
+
+//create onclick function for submit button
+// $( "(#ID for Submit button)" ).on( "click", function() {
+//   console.log( $( this ).text() );
+// });
+
+//
