15dbaa28982e60cf569ca4d30aec37706d4d8617
Shaun <torianharris917@gmail.com>
2019-01-19 09:36:03 -0500

    bug/clear-on-get-recipes

---
+++ Javascript/firebaseAuth.js
@@ -0,0 +1,49 @@
+var config = {
+  apiKey: "AIzaSyBa7iG9qCRNZE5o2RAzNB7IzyUbf8FOtH4",
+  authDomain: "whatsfordinner-88054.firebaseapp.com",
+  databaseURL: "https://whatsfordinner-88054.firebaseio.com",
+  projectId: "whatsfordinner-88054",
+  storageBucket: "whatsfordinner-88054.appspot.com",
+  messagingSenderId: "433727113101"
+};
+firebase.initializeApp(config);
+
+initApp = function() {
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var phoneNumber = user.phoneNumber;
+            var providerData = user.providerData;
+            user.getIdToken().then(function(accessToken) {
+              document.getElementById('sign-in-status').textContent = 'Signed in';
+              document.getElementById('sign-in').textContent = 'Sign out';
+              document.getElementById('account-details').textContent = JSON.stringify({
+                displayName: displayName,
+                email: email,
+                emailVerified: emailVerified,
+                phoneNumber: phoneNumber,
+                photoURL: photoURL,
+                uid: uid,
+                accessToken: accessToken,
+                providerData: providerData
+              }, null, '  ');
+            });
+          } else {
+            // User is signed out.
+            document.getElementById('sign-in-status').textContent = 'Signed out';
+            document.getElementById('sign-in').textContent = 'Sign in';
+            document.getElementById('account-details').textContent = 'null';
+          }
+        }, function(error) {
+          console.log(error);
+        });
+      };
+
+      window.addEventListener('load', function() {
+        initApp()
+      });
